554858b1fe315f2cceb4ae70c5c569789923dce1
Madhu-Kaushal <sudan4709@gmail.com>
2020-09-23 17:18:48 -0400

    temp

--- index.php
+++ index.php
@@ -1,49 +1,21 @@
 <?php
-require 'home2.html'; 
+require 'home.html'; 
 ?>
 
 <?
-
-#Receive user input
-$email_address = $_POST['email_address'];
-$feedback = $_POST['feedback'];
-
-#Filter user input
-function filter_email_header($form_field) {  
-return preg_replace('/[nr|!/<>^$%*&]+/','',$form_field);
-}
-
-$email_address  = filter_email_header($email_address);
-
-#Send email
-$headers = "From: $email_addressn";
-$sent = mail('you@domain.com', 'Feedback Form Submission', $feedback, $headers);
-
-#Thank user or notify them of a problem
-if ($sent) {
-
-?><html>
-<head>
-<title>Thank You</title>
-</head>
-<body>
-<h1>Thank You</h1>
-<p>Thank you for your feedback.</p>
-</body>
-</html>
-<?php
-
-} else {
-
-?><html>
-<head>
-<title>Something went wrong</title>
-</head>
-<body>
-<h1>Something went wrong</h1>
-<p>We could not send your feedback. Please try again.</p>
-</body>
-</html>
-<?php
+if (isset($_POST['submit')){
+	
+	$name = $_POST['your-name'];
+	$email = $_POST['your-email'];
+	$company = $_POST['your-address'];
+	$province = $_POST['Province'];
+	$message = $_POST['your-message-project'];
+	
+	$mailTo = "shruti_01@live.com";
+	$headers = "From: ".$email;
+	$txt = "Test Email".$name.".\n\n".$message;
+	
+	mail($mailTo, $headers, $txt);
+	header("Location: indexphp?mailsend");
 }
 ?>

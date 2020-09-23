516a39e01a3b3ee73e9aa7a48c9037be8fa40924
Madhu-Kaushal <sudan4709@gmail.com>
2020-09-23 17:28:19 -0400

    temp

--- index.php
+++ index.php
@@ -2,20 +2,3 @@
 require 'home.html'; 
 ?>
 
-<?php
-if (isset($_POST['submit')){
-	
-	$name = $_POST['your-name'];
-	$email = $_POST['your-email'];
-	$company = $_POST['your-address'];
-	$province = $_POST['Province'];
-	$message = $_POST['your-message-project'];
-	
-	$mailTo = "shruti_01@live.com";
-	$headers = "From: ".$email;
-	$txt = "Test Email".$name.".\n\n".$message;
-	
-	mail($mailTo, $headers, $txt);
-	header("Location: indexphp?mailsend");
-}
-?>

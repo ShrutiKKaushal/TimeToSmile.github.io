c542d848b9984a4f671ada29a0053d6fdd8197ff
Madhu-Kaushal <sudan4709@gmail.com>
2020-10-01 13:44:36 -0400

    testphp

--- feedback_form.php
+++ feedback_form.php
@@ -1,3 +1,26 @@
 <?php
 print_r($_POST);
+
+if (isset($_POST['submit']){
+	
+	$name = $_POST['your-name'];
+	$email = $_POST['your-email'];
+	$company = $_POST['your-address'];
+	$province = $_POST['Province'];
+	$message = $_POST['your-message-project'];
+	
+	print<"hr/>";
+	print "Name:$name<br />";
+	print "Email:$email<br />";
+	print "company:$company<br />";
+	print "Province:$province<br />";
+	print "message:$message<br />";
+	
+	$mailTo = "shruti_01@live.com";
+	$headers = "From: ".$email;
+	$txt = "Test Email".$name.".\n\n".$message;
+	
+	mail($mailTo, $headers, $txt);
+	header("Location: indexphp?mailsend");
+}
 ?>

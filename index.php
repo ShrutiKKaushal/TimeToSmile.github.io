ad27317f4dab10b61ae924aaeb80479ca0502581
Madhu-Kaushal <sudan4709@gmail.com>
2020-10-02 00:33:54 -0400

    testingphp

--- index.php
+++ index.php
@@ -2,3 +2,16 @@
 require 'home.html'; 
 ?>
 
+<?php
+
+echo json_encode($_POST);
+
+$name = $_POST['your-name'];
+	$email = $_POST['your-email'];
+	$company = $_POST['your-address'];
+	$province = $_POST['Province'];
+    $message = $_POST['your-message-project'];
+    
+    
+?>
+
